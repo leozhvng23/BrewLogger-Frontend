e7f933fc7525c7aa0d04b3e6022f808202eee673
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2022-07-30 17:41:26 -0400

    button for adding data

--- screens/AllRecipesOverviewScreen.js
+++ screens/AllRecipesOverviewScreen.js
@@ -1,14 +1,19 @@
 import { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { StyleSheet } from "react-native"
+import { StyleSheet } from "react-native";
 import { DrawerActions } from "@react-navigation/native";
 
 import IconButton from "../components/UIElements/IconButton";
+import AddButton from "../components/UIElements/AddButton";
 import RecipeList from "../components/RecipesList/RecipeList";
 import { RECIPES } from "../data/dummy";
 
 const AllRecipesOverviewScreen = () => {
 	const navigation = useNavigation();
+
+	const addRecipeHandler = () => {
+		console.log("added!")
+	}
 
 	useLayoutEffect(() => {
 		navigation.setOptions({
@@ -21,6 +26,7 @@ const AllRecipesOverviewScreen = () => {
 					/>
 				);
 			},
+			headerRight: () => <AddButton onAddData={addRecipeHandler}/>,
 		});
 	}, [navigation]);
 
