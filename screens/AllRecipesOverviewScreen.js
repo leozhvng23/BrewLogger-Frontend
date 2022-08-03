94cf02fae7fe371244dd97ae45c291bc70391671
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2022-08-02 21:49:46 -0400

    fetches data from backend

--- screens/AllRecipesOverviewScreen.js
+++ screens/AllRecipesOverviewScreen.js
@@ -1,19 +1,32 @@
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { StyleSheet } from "react-native";
 import { DrawerActions } from "@react-navigation/native";
+import { useDispatch, useSelector } from "react-redux";
 
 import IconButton from "../components/UIElements/IconButton";
 import AddButton from "../components/UIElements/AddButton";
 import RecipeList from "../components/RecipesList/RecipeList";
-import { RECIPES } from "../data/dummy";
+import { getRecipesByUserId } from "../util/http";
+import { setRecipes } from "../store/redux/recipes";
 
 const AllRecipesOverviewScreen = () => {
+
+	const uid = useSelector((state) => state.user.uid);
+
 	const navigation = useNavigation();
+	const dispatch = useDispatch();
 
 	const addRecipeHandler = () => {
-		navigation.navigate("ManageRecipe")
-	}
+		navigation.navigate("ManageRecipe");
+	};
+	useEffect(() => {
+		const fetchRecipes = async () => {
+			const recipes = await getRecipesByUserId(uid);
+			dispatch(setRecipes({recipes: recipes}));
+		};
+		fetchRecipes();
+	}, []);
 
 	useLayoutEffect(() => {
 		navigation.setOptions({
@@ -26,11 +39,13 @@ const AllRecipesOverviewScreen = () => {
 					/>
 				);
 			},
-			headerRight: () => <AddButton onAddData={addRecipeHandler}/>,
+			headerRight: () => <AddButton onAddData={addRecipeHandler} />,
 		});
 	}, [navigation]);
 
-	return <RecipeList items={RECIPES} />;
+	const recipes = useSelector((state) => state.recipes.recipes)
+
+	return <RecipeList items={recipes} />;
 };
 
 export default AllRecipesOverviewScreen;
