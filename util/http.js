877415ccc47b2475b7037cb26d755da1049bbf73
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2022-08-02 21:47:39 -0400

    added get recipes request to backend

---
+++ util/http.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const BACKEND_URL = "http://192.168.1.142:5500";
+const USERS_API = "/api/users";
+const RECIPES_API = "/api/recipes";
+
+
+export async function getRecipesByUserId(uid) {
+	const recipes = [];
+	console.log(BACKEND_URL + RECIPES_API + "/user/" + uid);
+	const response = await axios.get(BACKEND_URL + RECIPES_API + "/user/" + uid);
+
+	for (const key in response.data) {
+		const recipeObj = {
+			id: response.data[key].id,
+			name: response.data[key].name,
+			photo_url: response.data[key].photo_url,
+			type: response.data[key].type,
+			brew_time: response.data[key].brew_time,
+			brewer: response.data[key].brewer,
+		};
+		recipes.push(recipeObj);
+	}
+    console.log(recipes);
+	return recipes;
+}
