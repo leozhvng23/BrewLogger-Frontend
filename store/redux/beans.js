9e2f712818fdeb7bb25800f204a6bc2defda66a4
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2022-08-04 15:17:18 -0400

    added reducers

--- store/redux/beans.js
+++ store/redux/beans.js
@@ -1,23 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const beanSlice = createSlice({
-	name: "recipes",
+	name: "beans",
 	initialState: { 
-        beans: [],
+        beans: {},
         fetchedIds: [],
      },
 	reducers: {
-		setBeans: (state, action) => {
+		setBeansNames: (state, action) => {
 			state.beans = action.payload.beans;
 		},
-        setBeansDetail: (state, action) => {
+        setBeanDetail: (state, action) => {
             state.beans[action.payload.bid] = action.payload.beanDetail;
             state.fetchedIds.push(action.payload.bid)
         }
 	},
 });
 
-export const setBeans = beanSlice.actions.setBeans;
+export const setBeansNames = beanSlice.actions.setBeansNames;
 export const setBeanDetail = beanSlice.actions.setBeanDetail;
 
 export default beanSlice.reducer;
---
+++ store/redux/equipments.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const equipmentSlice = createSlice({
+	name: "beans",
+	initialState: {
+		brewers: {},
+		grinders: {},
+		fetchedBrewerIds: [],
+		fetchedGrinderIds: [],
+	},
+	reducers: {
+		setBrewersNames: (state, action) => {
+			state.brewers = action.payload.brewers;
+		},
+		setBrewerDetail: (state, action) => {
+			state.brewers[action.payload.eid] = action.payload.brewerDetail;
+			state.fetchedBrewerIds.push(action.payload.eid);
+		},
+		setGrindersNames: (state, action) => {
+			state.grinders = action.payload.grinders;
+		},
+		setGrinderDetail: (state, action) => {
+			state.grinders[action.payload.eid] = action.payload.grinderDetail;
+			state.fetchedGrinderIds.push(action.payload.eid);
+		},
+	},
+});
+
+export const setBrewersNames = equipmentSlice.actions.setBrewersNames;
+export const setBrewerDetail = equipmentSlice.actions.setBrewerDetail;
+export const setGrindersNames = equipmentSlice.actions.setGrindersNames;
+export const setGrinderDetail = equipmentSlice.actions.setGrinderDetail;
+
+export default equipmentSlice.reducer;
