69ba59e423938557964dbd221476b70081c8e9e5
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2022-08-09 15:51:12 -0400

    Feed recipes wip

--- store/redux/recipes.js
+++ store/redux/recipes.js
@@ -2,26 +2,32 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const recipeSlice = createSlice({
 	name: "recipes",
-	initialState: { 
-        recipes: {},
-        // fetchedIds: [], // recipes with all data
-        myIds: [], // my created recipes
-        sharedWithMeIds: [], // shared with me recipes
-        likedIds: [] // liked recipes
-     },
+	initialState: {
+		recipes: {},
+		// fetchedIds: [], // recipes with all data
+		myIds: [], // my created recipes
+		sharedWithMeIds: [], // shared with me recipes
+		likedIds: [], // liked recipes
+	},
 	reducers: {
 		setRecipes: (state, action) => {
 			state.recipes = action.payload.recipes;
-            state.myIds = action.payload.ids;
+			state.myIds = action.payload.ids;
 		},
-        // setRecipeDetail: (state, action) => {
-        //     state.recipes[action.payload.id] = action.payload.recipeDetail;
-        //     state.fetchedIds.push(action.payload.id)
-        // },
+		setFeedRecipes: (state, action) => {
+			for (const [id, recipe] of Object.entries(action.payload.recipes)) {
+				state.recipes[id] = recipe;
+			}
+		},
+		setRecipeDetail: (state, action) => {
+		    state.recipes[action.payload.id] = action.payload.recipeDetail;
+		    state.fetchedIds.push(action.payload.id)
+		},
 	},
 });
 
 export const setRecipes = recipeSlice.actions.setRecipes;
-// export const setRecipeDetail = recipeSlice.actions.setRecipeDetail;
+export const setFeedRecipes = recipeSlice.actions.setFeedRecipes;
+export const setRecipeDetail = recipeSlice.actions.setRecipeDetail;
 
 export default recipeSlice.reducer;
