d3fe66dd989cb150b7d73ba5f6548683f359c3f6
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2022-08-15 16:50:34 -0400

    changed logged in user to Leo

--- store/redux/user.js
+++ store/redux/user.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
 	name: "user",
-	initialState: { uid : 6, username: "Bot" },
+	initialState: { uid : 6, username: "Leo" },
 	reducers: {
 	},
 });
